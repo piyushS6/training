061b1cdf76b4f8e9e5d1df7b22a43fdd327e1438
piyush <piyush.sonawane@launchventures.co>
2023-09-25 13:58:26 +0530

    FED exercise

---
+++ FED/Dashboard/script.js
@@ -0,0 +1,19 @@
+// sidebar toggle
+
+var sidebarOpen = false;
+var sidebar = document.getElementsByClassName('sidebar');
+console.log(sidebar);
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar-responsive');
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar-responsive');
+    sidebarOpen = false;
+  }
+}
